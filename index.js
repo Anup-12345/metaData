39a1c03e0320905641e7cbe88b8b55767bbdb3a9
Anup-12345 <anup4867@gmail.com>
2026-08-12 22:37:16 +0530

    fix minoior issue

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
 
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.get('/hello', function (req, res) {
+  res.json({
+    greetings: 'Hello, API'
+  });
+});
+
+// File Metadata API
+app.post(
+  '/api/fileanalyse',
+  multer().single('upfile'),
+  function (req, res) {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  }
+);
+
+// Start server LAST
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
-
-let multer = require('multer')
-
-app.post('/api/fileanalyse', multer().single('upfile'), (request, response) => {
-  let responseObject = {}
-  responseObject['name'] = request.file.originalname
-  responseObject['type'] = request.file.mimetype
-  responseObject['size'] = request.file.size
-  
-  response.json(responseObject)
-} )
